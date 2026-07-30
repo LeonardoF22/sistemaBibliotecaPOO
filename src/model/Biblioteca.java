68a37740a3420f6e1b0781c037a25cb1e15e7c88
LeonardoF22 <leonardoferodrigues7@gmail.com>
2026-07-30 09:23:57 -0300

    add classe Biblioteca

---
+++ src/model/Biblioteca.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Biblioteca {
+    private Livro[] livrosCadastrados = new Livro[100];
+    private Usuario[] usuariosCadastrados = new Usuario[100];
+    private int quantidadeDeLivros;
+    private int quantidadeDeUsuarios;
+}
